45d4cf564fc272e2a51117736e6b9f0b83260f53
Daniel Lohse <info@asapdesign.de>
2010-10-06 01:26:12 +0200

    1) provide the template the current $page object and 2) handle empty slot content

--- modules/adHTMLWrapperSlot/templates/_normalView.php
+++ modules/adHTMLWrapperSlot/templates/_normalView.php
@@ -9,10 +9,11 @@
     <?php echo __('Please switch into edit mode to add content to this slot.', null, 'apostrophe') ?>
   <?php endif ?>
 <?php elseif ((array_key_exists('text', $values) && strlen($values['text']) > 0) || (isset($options['optional']) && $options['optional'])): ?>
+  <?php $text = array_key_exists('text', $values) ? $values['text'] : '' ?>
   <?php if (false !== $itemTemplate): ?>
-    <?php echo str_replace('[[TEXT]]', get_partial('adHTMLWrapperSlot/'.$itemTemplate, array('text' => $values['text'], 'name' => $name, 'permid' => $permid, 'pageid' => $pageid, 'slot' => $slot)), $wrap) ?>
+    <?php echo str_replace('[[TEXT]]', get_partial('adHTMLWrapperSlot/'.$itemTemplate, array('text' => $text, 'name' => $name, 'permid' => $permid, 'pageid' => $pageid, 'page' => $page, 'slot' => $slot)), $wrap) ?>
   <?php else: ?>
     <?php /* TODO!!! Handle this nl2br business some other way: take a look at aText and a::textToHTML (or so :)) */ ?>
-    <?php echo nl2br(str_replace('[[TEXT]]', $values['text'], $wrap)) ?>
+    <?php echo nl2br(str_replace('[[TEXT]]', $text, $wrap)) ?>
   <?php endif ?>
 <?php endif ?>
